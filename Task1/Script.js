b3b39aa00c6aad53b3451cf575a6d403a8ab4644
nikhilakarri3108 <nikhilakarri3108@gmail.com>
2026-06-21 16:31:25 +0530

    Create Script.js

---
+++ Task1/Script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const welcomeBtn = document.getElementById("welcomeBtn");
+
+  welcomeBtn.addEventListener("click", () => {
+    alert("Welcome to Vignan! 🎓✨");
+  });
+});
